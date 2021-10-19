526702d5da68d29084937fc41657a9be6c96f60b
Akash Kejriwal <akash@kejriwal.com>
2021-10-19 00:09:59 -0400

    Init bootstrap layout for budget application

--- src/index.js
+++ src/index.js
@@ -2,8 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+class Budget extends React.Component {
+	render() {
+		return (
+			<Container fluid>
+				<Row>
+					<Col>
+						<TransactionsTable />
+					</Col>
+					<Col>
+						<TransactionForm />
+					</Col>
+				</Row>
+			</Container>
+		)
+	}
+}
+
+class Transaction extends React.Component {
+
+}
+
+class TransactionsTable extends React.Component {
+	render() {
+		return null
+	}
+}
+
+class TransactionForm extends React.Component {
+	render() {
+		return null
+	}
+}
+
+// ========================================
+
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Budget />,
+	document.getElementById('root')
 );
+
+
